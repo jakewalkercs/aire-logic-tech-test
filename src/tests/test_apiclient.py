aa8ace791e6853ba063a8a57b60762f43b7ea1f1
Jake Walker <ll17j5w@leeds.ac.uk>
2022-09-16 23:17:45 +0100

    test

--- src/tests/test_apiclient.py
+++ src/tests/test_apiclient.py
@@ -1,6 +1,9 @@
 """Main test contents"""
 
-from src.apiclient import get_artist_mbid
+import sys
+sys.path.insert(0, '../src')
+from apiclient import get_artist_mbid
+
 
 def test_get_artist_mbid():
     """Simple test to ensure the API returns an artist"""
