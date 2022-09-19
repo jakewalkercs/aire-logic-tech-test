6edd93e7aff838b799893282c27d3a794f77442d
Jake Walker <ll17j5w@leeds.ac.uk>
2022-09-19 21:05:14 +0100

    feature/logging dev complete

--- src/apiclient.py
+++ src/apiclient.py
@@ -5,20 +5,25 @@ import xmltodict
 
 
 def _get(log_object, uri, payload,headers=None, retry=False):
-    # log_object.info('Attempting API call to {}'.format(uri))
+    """Simple API client to retry a get request once."""
+
+    log_object.info('Attempting API call to {}'.format(uri))
 
     response = requests.get(uri, headers=headers, params=payload)
 
     if retry or response.status_code == requests.codes.ok:
         return response
 
-    # log_object.info(
-    #     'API call to {} returned error {}'.format(
-    #         uri, response.status_code))
+    log_object.info(
+        'API call to {} returned error {}'.format(
+            uri, response.status_code))
     return _get(log_object, uri, payload, retry=True)
 
 
 def get_artist_albums(log_object, artist):
+    """Takes an artist name and uses the music brainz API to return
+    all albums and singles by the artist"""
+
     uri = "http://musicbrainz.org/ws/2/release-group"
     payload = {'query': "'artist': '%s'" % (artist), 'fmt': 'json'}
 
@@ -29,12 +34,15 @@ def get_artist_albums(log_object, artist):
     #         'API call to {} returned error {}'.format(
     #             uri, response.status_code))
 
-    # log_object.info('Successfully returned artist albums')
+    log_object.info('Successfully returned artist albums')
 
     return response.json()
 
 
 def get_artist_songs(log_object, album, artist):
+    """Takes an artist name and album and uses the last.fm to return
+    all songs from an album by a particular artist"""
+
     API_KEY = '10af4229b7d1a09adc7029eed6814cce'
     USER_AGENT = 'Dataquest'
     uri = 'https://ws.audioscrobbler.com/2.0/'
@@ -55,12 +63,15 @@ def get_artist_songs(log_object, album, artist):
     #         'API call to {} returned error {}'.format(
     #             uri, response.status_code))
 
-    # log_object.info('Successfully returned artist songs')
+    log_object.info('Successfully returned artist songs')
 
     return response.json()
 
 
 def get_artist_song_lyrics(log_object, song, artist):
+    """Takes a song name and artist name and returns all of the
+    lyrics from that song"""
+
     uri = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect"
 
     payload = {
@@ -70,12 +81,12 @@ def get_artist_song_lyrics(log_object, song, artist):
 
     response = _get(log_object, uri, payload)
 
-    if response.status_code != requests.codes.ok:
-        raise requests.HTTPError(
-            'API call to {} returned error {}'.format(
-                uri, response.status_code))
+    # if response.status_code != requests.codes.ok:
+    #     raise requests.HTTPError(
+    #         'API call to {} returned error {}'.format(
+    #             uri, response.status_code))
 
-    # log_object.info('Successfully returned artist songs')
+    log_object.info('Successfully returned artist songs')
 
     xml_converted = xmltodict.parse(response.text)
 
